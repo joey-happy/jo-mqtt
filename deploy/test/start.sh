7f0a0bacee652f1d439dd0a43242dff0e34fa415
yaozhichao <email>
2021-03-23 14:22:25 +0800

    feat(deploy): 添加部署脚本

---
+++ deploy/test/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+JAVA_MEM_OPTS="-Xms4g -Xmx4g -XX:MetaspaceSize=512m -XX:MaxMetaspaceSize=1024m -XX:NewRatio=1 -XX:SurvivorRatio=8 "
+
+JAVA_GC_OPTS="-XX:+ExplicitGCInvokesConcurrent -XX:MaxTenuringThreshold=6 -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:CMSInitiatingOccupancyFraction=70 -XX:+UseCMSInitiatingOccupancyOnly "
+
+JAVA_DUMP_OPTS="-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/home/logs/jo-mqtt/ "
+
+JAVA_LOG_OPTS="-XX:+PrintCommandLineFlags -Xloggc:/home/logs/jo-mqtt/gc.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCApplicationStoppedTime -XX:ErrorFile=/home/logs/jo-mqtt/errorPid.log "
+
+JAVA_JMX_OPTS="-Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=1899 -Dcom.sun.management.jmxremote.ssl=false "
+
+JAVA_OTHER_OPTS="-Dfile.encoding=UTF-8 -Duser.timezone=GMT+08 -XX:+UseCompressedOops -XX:+ParallelRefProcEnabled "
+
+JAVA_OPTS="$JAVA_MEM_OPTS $JAVA_GC_OPTS $JAVA_DUMP_OPTS $JAVA_LOG_OPTS $JAVA_JMX_OPTS $JAVA_OTHER_OPTS "
+
+nohup java -jar $JAVA_OPTS -Dserver.port=7788 -Dspring.profiles.active=test -Dmqtt.nettyConfig.epoll=false jo-mqtt-broker.jar>console.log 2>&1 &
+echo "$!" > run.pid
+echo "mqtt启动完成!"
---
+++ deploy/test/stop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+kill `cat run.pid`
